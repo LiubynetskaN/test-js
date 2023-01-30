1ef7b6a5545a08c06fede8d5b5eebda35dc54a6a
Nataliia Liubynetska <nliubynetska@gmail.com>
2023-01-30 20:07:43 +0200

    'задача 1

---
+++ js/test-1.js
@@ -0,0 +1,18 @@
+const userInfo = Number(prompt("Введіть число від 0 до 60"));
+
+if (userInfo >= 0 && userInfo <= 15) {
+    alert("Ваше число знаходиться у 1-й чверті");
+} else if (userInfo >= 16 && userInfo <= 30) {
+    alert("Ваше число знаходиться у 2-й чверті");
+} else if (userInfo >= 31 && userInfo <= 45) {
+    alert("Ваше число знаходиться у 3-й чверті");
+} else if (userInfo >= 46 && userInfo <= 60) {
+    alert("Ваше число знаходиться у 4-й чверті");
+} else if (isNaN(userInfo)) {
+    alert( "Введіть, будь-ласка, число");
+} else {
+    alert("Ваші дані не валідні")};
+
+
+console.log(typeof userInfo);
+console.log(userInfo);
